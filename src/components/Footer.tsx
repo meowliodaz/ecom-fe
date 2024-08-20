c919e571f4526b539ee9338e72d26ca70c87cb90
Trieu <trieuhaipham32621998@gmail.com>
2024-08-20 12:28:21 +0700

    :sparkles: [comp] Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<div className="bg-gray-200 sticky bot-0 inset-x-0 h-16 flex mt-10">
+			<p
+				className='m-auto font-serif'
+			>
+				This is a product of Pham Hai Trieu.
+			</p>
+		</div>
+	)
+}
+
+export default Footer
